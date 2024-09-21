d3369efc88932d15294710d889311c655f6d7ac3
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-09-21 09:48:32 +0700

    Add empty CSS files for home, about, queqe, and contact components

---
+++ src/app/queqe/queqe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-queqe',
+  standalone: true,
+  imports: [],
+  templateUrl: './queqe.component.html',
+  styleUrl: './queqe.component.css'
+})
+export class QueqeComponent {
+
+}
